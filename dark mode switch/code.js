959651d94ca6f94bb23753301a424e2cb5548b31
pouriversal <pooriversal@gmail.com>
2022-03-22 17:02:59 +0430

    add support for strokeId and effectId + adj functionality

--- dark mode switch/code.js
+++ dark mode switch/code.js
@@ -1,23 +1,51 @@
 // @ts-nocheck
+let numebr_of_items = 0;
+const selected = figma.currentPage.selection;
+const styleList = figma.getLocalPaintStyles();
+let changed = false;
 function runPlugin() {
-    let numebr_of_items = 0;
-    const selected = figma.currentPage.selection;
-    const styleList = figma.getLocalPaintStyles();
+    console.log(selected);
     if (selected.length === 0) {
         figma.closePlugin('Please select some valid objects');
     }
     for (let item of selected) {
-        if (item.fillStyleId) {
+        changeId(item);
+        if (item.children && item.children.length > 0) {
+            for (let child of item.children) {
+                changeId(child);
+            }
+        }
+        if (changed) {
             numebr_of_items++;
-            const styleName = styleList.filter(style => style.id === item.fillStyleId)[0].name;
-            const is_dark = styleName.includes('dark') ? 'dark' : 'light';
-            const new_id = styleList.filter(style => style.name === styleName.replace(is_dark, is_dark === 'dark' ? 'light' : 'dark'))[0].id;
-            item.fillStyleId = new_id;
+            changed = false;
         }
     }
     if (numebr_of_items === 0) {
         figma.closePlugin('Select objects with fill styles which has "dark" or "light" in its name');
     }
     figma.closePlugin(`${numebr_of_items} items have been updated`);
+}
+function changeId(selected) {
+    if (selected.fillStyleId) {
+        changed = true;
+        const styleName = styleList.filter(style => style.id === selected.fillStyleId)[0].name;
+        const is_dark = styleName.includes('dark') ? 'dark' : 'light';
+        const new_id = styleList.filter(style => style.name === styleName.replace(is_dark, is_dark === 'dark' ? 'light' : 'dark'))[0].id;
+        selected.fillStyleId = new_id;
+    }
+    if (selected.effectStyleId) {
+        changed = true;
+        const styleName = styleList.filter(style => style.id === selected.effectStyleId)[0].name;
+        const is_dark = styleName.includes('dark') ? 'dark' : 'light';
+        const new_id = styleList.filter(style => style.name === styleName.replace(is_dark, is_dark === 'dark' ? 'light' : 'dark'))[0].id;
+        selected.effectStyleId = new_id;
+    }
+    if (selected.strokeStyleId) {
+        changed = true;
+        const styleName = styleList.filter(style => style.id === selected.strokeStyleId)[0].name;
+        const is_dark = styleName.includes('dark') ? 'dark' : 'light';
+        const new_id = styleList.filter(style => style.name === styleName.replace(is_dark, is_dark === 'dark' ? 'light' : 'dark'))[0].id;
+        selected.strokeStyleId = new_id;
+    }
 }
 runPlugin();
